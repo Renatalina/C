b609e99fb26693c81b11bd27e003f4dc5dbac4e1
natalia_viskovatykh@epam.com <natalia_viskovatykh@epam.com>
2024-03-31 22:31:08 +0300

    Variables of C

---
+++ c_variables.c
@@ -0,0 +1,15 @@
+# include <stdio .h>
+
+ int main ( void )
+{
+ printf (" value %zu\n", sizeof (5));
+  printf (" value %zu\n", sizeof (5 L ));
+ printf (" value %zu\n", sizeof (5 LL ));
+printf (" value %zu\n", sizeof (5.0));
+printf (" value %zu\n", sizeof (5.0 F ));
+printf (" value %zu\n", sizeof (5.0 L )); 
+printf (" value %zu\n", sizeof (’x’));
+
+return 0;
+}
+ 
