2620bd9876b502c33a8041212ab76a68584c7d99
NatalinaViskovatykh <Natalia_viskovatykh@epam.com>
2024-05-27 09:12:08 +0300

    A little update task

--- task6.2/main.c
+++ task6.2/main.c
@@ -4,6 +4,55 @@
 #include <string.h>
 #include <vadefs.h>
 
+// #define MAX_LENGHT  100
+
+// typedef struct stack_t {
+//     char name[MAX_LENGHT];
+//     char lastname[MAX_LENGHT];
+//     struct stack_t *prev_elem;    
+// } stack_t;
+
+// stack_t stack_new ( void ) {
+//     return (stack_t){};
+// }
+
+// bool stack_empty ( stack_t stk ) {
+//     return stk.name != NULL ? 1 : 0;
+// }
+
+// void stack_push ( stack_t *stk , void *x) {
+//     stk->prev_elem=x;
+// }
+
+// void *stack_pop ( stack_t stk) {
+    
+// }
+
+// void stack_free ( stack_t *stk ) {
+//     free(stk);
+// }
+
+// int main(){    
+//     stack_t stack=stack_new();
+
+//     stack_t new_elem={"Natalia", "Viski"};
+//     stack_t new_elem2={"Elen", "Viski"};
+//     stack_t new_elem3={"Valent", "Viski"};
+
+//     stack_push(&new_elem, &stack);
+//     stack_push(&new_elem2, &new_elem);
+//     stack_push(&new_elem3, &new_elem3);
+//     printf("%s\n", new_elem2.prev_elem->name);
+
+
+
+//     stack_free(&new_elem3);
+//     stack_free(&new_elem);
+
+//     return 0;
+// }
+
+
 #define MAX_LENGHT  100
 
 typedef struct stack_t {
@@ -12,27 +61,59 @@ typedef struct stack_t {
     struct stack_t *prev_elem;    
 } stack_t;
 
-stack_t stack_new ( void ) {
-    return (stack_t){"Natalia", "Viski"};
+stack_t *stack_new ( void ) {
+    stack_t* stk = calloc(1, sizeof(stack_t)); 
+    return stk;
 }
 
-bool stack_empty ( stack_t stk ) {
-    return stk.name == "Natalia" && stk.lastname == "Viski" ? 1 : 0;
+bool stack_empty ( stack_t *stk ) {
+    return stk == NULL;
 }
 
-void stack_push ( stack_t stk , void *x) {
-    stk.prev_elem=x;
+void stack_push ( stack_t **stk , stack_t *x) {
+    x->prev_elem = *stk;
+    *stk = x;
 }
 
-void *stack_pop ( stack_t stk, stack_t next ) {
-    next.prev_elem = stk.prev_elem;
+stack_t *stack_pop ( stack_t **stk ) {
+    if(stack_empty(*stk)) {
+        return NULL;
+    }
+    stack_t* top = *stk;
+    *stk = (*stk)->prev_elem;  
+    return top;
 }
 
 void stack_free ( stack_t *stk ) {
-    free(stk);
+    while(stk != NULL) {
+        stack_t* prev = stk->prev_elem;
+        free(stk);
+        stk = prev;
+    }
 }
 
-int main(){
+int main(){    
+    stack_t *stack = stack_new();
     
+    stack_t *new_elem = stack_new();
+    strcpy(new_elem->name, "Natalia");
+    strcpy(new_elem->lastname, "Viski");
+    
+    stack_t *new_elem2 = stack_new();
+    strcpy(new_elem2->name, "Elen");
+    strcpy(new_elem2->lastname, "Viski");
+
+    stack_push(&stack, new_elem);
+    stack_push(&stack, new_elem2);
+
+    printf("Stack top: %s\n", stack->name);
+
+    stack_pop(&stack);  
+
+    if(stack != NULL) {
+        printf("Stack top after pop: %s\n", stack->name);
+    }
+
+    stack_free(stack);
     return 0;
 }
