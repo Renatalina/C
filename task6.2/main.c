0fd96e8b11810bedadce91a930855989d5cf15c5
NatalinaViskovatykh <Natalia_viskovatykh@epam.com>
2024-06-04 17:33:06 +0300

    New version

--- task6.2/main.c
+++ task6.2/main.c
@@ -119,71 +119,132 @@
 //     return 0;
 // }
 
+
+////work version
+// #define MAX_LENGHT  100
+
+// typedef struct stack_t {
+//     char name[MAX_LENGHT+1];
+//     char lastname[MAX_LENGHT+1];
+//     struct stack_t *prev_elem;    
+// } *stack_t;
+
+// stack_t stack_new ( void ) {
+//     stack_t stk = calloc(1, sizeof(struct stack_t)); 
+//     return stk;
+// }
+
+// bool stack_empty ( stack_t stk ) {
+//     return stk == NULL;
+// }
+
+// void stack_push ( stack_t stk , void *x) {
+//     stk->prev_elem = x;
+// }
+
+// void *stack_pop ( stack_t stk ) {
+//     if(stack_empty(stk)) {
+//         return NULL;
+//     }
+
+//     stack_t top = stk;
+//     free(stk);
+//     stk = top->prev_elem;
+// }
+
+// void stack_free ( stack_t* stk ) {
+//     while(stk != NULL) {
+//         free(stk);
+//     }
+// }
+
+// int main() {
+
+//     stack_t stack = stack_new();
+    
+//     stack_t new_elem = stack_new();
+//     strcpy(new_elem->name, "Natalia");
+//     strcpy(new_elem->lastname, "Viski");
+//     stack_push(stack, new_elem);
+    
+    
+//     stack_t new_elem2 = stack_new();
+//     strcpy(new_elem2->name, "Elen");
+//     strcpy(new_elem2->lastname, "Viski");
+//     stack_push(new_elem, new_elem2);
+
+//     stack_t new_elem3 = stack_new();
+//     strcpy(new_elem3->name, "Valent");
+//     strcpy(new_elem3->lastname, "Viski");    
+//     stack_push(new_elem2, new_elem3);
+
+//     printf("Stack top: %s\n", stack->prev_elem->name);
+
+//     stack = stack_pop(stack); 
+//     stack = stack_pop(stack); 
+
+//     if(stack != NULL) {
+//         printf("Stack top after pop: %s\n", stack->name);
+//         free(stack);
+//     }
+
+//     return 0;
+// }
+
+
 #define MAX_LENGHT  100
 
 typedef struct stack_t {
-    char name[MAX_LENGHT+1];
-    char lastname[MAX_LENGHT+1];
-    struct stack_t *prev_elem;    
-} *stack_t;
+    void* data;
+    struct stack_t *prev_elem;
+}stack_t;
 
-stack_t stack_new ( void ) {
-    stack_t stk = calloc(1, sizeof(struct stack_t)); 
-    return stk;
+bool stack_empty(stack_t* stk) {
+    return stk==NULL;
 }
 
-bool stack_empty ( stack_t stk ) {
-    return stk == NULL;
+stack_t* stack_push(stack_t* stk, void *new_data) {
+    stack_t* new_elem = calloc(1,sizeof(stack_t));
+    new_elem->data = new_data;
+    new_elem->prev_elem = stk;
+    return new_elem;    
 }
 
-void stack_push ( stack_t stk , void *x) {
-    stk->prev_elem = (struct stack_t *) x;
-}
-
-void *stack_pop ( stack_t stk ) {
-    if(stack_empty(stk)) {
+stack_t* stack_pop(stack_t* stk) {
+    if(stack_empty(stk)){
         return NULL;
     }
-
-    stack_t top = stk;
-    free(stk);
-    stk = top->prev_elem;
+    stack_t *popped_elem = stk;
+    stk = stk->prev_elem;
+    return stk; 
 }
 
-void stack_free ( stack_t* stk ) {
-    while(stk != NULL) {
-        free(stk);
+void stack_free(stack_t* stk) {
+    stack_t* current=stk;
+    while(current != NULL){
+        stack_t* next=current->prev_elem;
+        free(current);
+        current=next;
     }
 }
 
 int main() {
+ 
+    stack_t* my_stack = NULL;
 
-    stack_t stack = stack_new();
-    
-    stack_t new_elem = stack_new();
-    strcpy(new_elem->name, "Natalia");
-    strcpy(new_elem->lastname, "Viski");
-    stack_push(stack, new_elem);
-    
-    
-    stack_t new_elem2 = stack_new();
-    strcpy(new_elem2->name, "Elen");
-    strcpy(new_elem2->lastname, "Viski");
-    stack_push(new_elem, new_elem2);
+    for (int i = 0; i < 10; ++i) {
+        int* data = (int*) malloc(sizeof(int));
+        *data = i;
+        my_stack = stack_push(my_stack, data);
+    }
 
-    stack_t new_elem3 = stack_new();
-    strcpy(new_elem3->name, "Valent");
-    strcpy(new_elem3->lastname, "Viski");    
-    stack_push(new_elem2, new_elem3);
-
-    printf("Stack top: %s\n", stack->prev_elem->name);
-
-    stack = stack_pop(stack); 
-    stack = stack_pop(stack); 
-
-    if(stack != NULL) {
-        printf("Stack top after pop: %s\n", stack->name);
-        free(stack);
+    while (!stack_empty(my_stack)) {
+        int* data = (int*) my_stack->data;
+        printf("%d\n", *data);
+        stack_t* next_stack = stack_pop(my_stack);
+        free(data);
+        free(my_stack);
+        my_stack = next_stack;
     }
 
     return 0;
