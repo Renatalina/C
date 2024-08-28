e97b6354985fc846bc4c59835f8b5ebfbb2613bd
NatalinaViskovatykh <Natalia_viskovatykh@epam.com>
2024-08-28 11:20:21 +0300

    Update task

--- FINISH/success.c
+++ FINISH/success.c
@@ -4,21 +4,18 @@
 
 #define MAX_TREE_HT 256
 
-// Узел дерева Хаффмана
 struct MinHeapNode {
     char data;
     unsigned freq;
     struct MinHeapNode *left, *right;
 };
 
-// Структура минимальной кучи (очереди)
 struct MinHeap {
     unsigned size;
     unsigned capacity;
     struct MinHeapNode** array;
 };
 
-// Создание нового узла дерева Хаффмана
 struct MinHeapNode* newNode(char data, unsigned freq) {
     struct MinHeapNode* temp = (struct MinHeapNode*)malloc(sizeof(struct MinHeapNode));
     temp->left = temp->right = NULL;
@@ -27,7 +24,6 @@ struct MinHeapNode* newNode(char data, unsigned freq) {
     return temp;
 }
 
-// Создание минимальной кучи (очереди)
 struct MinHeap* createMinHeap(unsigned capacity) {
     struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
     minHeap->size = 0;
@@ -36,32 +32,26 @@ struct MinHeap* createMinHeap(unsigned capacity) {
     return minHeap;
 }
 
-// Обмен двух узлов
 void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b) {
     struct MinHeapNode* t = *a;
     *a = *b;
     *b = t;
 }
 
-// Просеивание вниз для поддержания структуры минимальной кучи
 void minHeapify(struct MinHeap* minHeap, int idx) {
     int smallest = idx;
     int left = 2 * idx + 1;
     int right = 2 * idx + 2;
-
     if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
         smallest = left;
-
     if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
         smallest = right;
-
     if (smallest != idx) {
         swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
         minHeapify(minHeap, smallest);
     }
 }
 
-// Вытягивание минимального узла из кучи
 struct MinHeapNode* extractMin(struct MinHeap* minHeap) {
     struct MinHeapNode* temp = minHeap->array[0];
     minHeap->array[0] = minHeap->array[minHeap->size - 1];
@@ -70,7 +60,6 @@ struct MinHeapNode* extractMin(struct MinHeap* minHeap) {
     return temp;
 }
 
-// Вставка узла в кучу
 void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode) {
     ++minHeap->size;
     int i = minHeap->size - 1;
@@ -81,129 +70,118 @@ void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode) {
     minHeap->array[i] = minHeapNode;
 }
 
-// Создание минимальной кучи и добавление узлов
-struct MinHeap* buildMinHeap(char data[], int freq[], int size) {
-    struct MinHeap* minHeap = createMinHeap(size);
-    for (int i = 0; i < size; ++i)
-        minHeap->array[i] = newNode(data[i], freq[i]);
-    minHeap->size = size;
+struct MinHeap* buildMinHeapFromFreq(int freq[]) {
+    struct MinHeap* minHeap = createMinHeap(256);
+    for (int i = 0; i < 256; ++i) {
+        if (freq[i] > 0) {
+            minHeap->array[minHeap->size] = newNode((char)i, freq[i]);
+            minHeap->size++;
+        }
+    }
     for (int i = (minHeap->size - 1) / 2; i >= 0; --i)
         minHeapify(minHeap, i);
     return minHeap;
 }
 
-// Построение дерева Хаффмана
-struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size) {
-    struct MinHeapNode *left, *right, *top;
-    struct MinHeap* minHeap = buildMinHeap(data, freq, size);
-
+struct MinHeapNode* buildHuffmanTree(int freq[]) {
+    struct MinHeap* minHeap = buildMinHeapFromFreq(freq);
     while (minHeap->size != 1) {
-        left = extractMin(minHeap);
-        right = extractMin(minHeap);
-
-        top = newNode('$', left->freq + right->freq);
+        struct MinHeapNode *left = extractMin(minHeap), *right = extractMin(minHeap);
+        struct MinHeapNode *top = newNode('$', left->freq + right->freq);
         top->left = left;
         top->right = right;
         insertMinHeap(minHeap, top);
     }
-
     return extractMin(minHeap);
 }
 
-// Печать кодов Хаффмана (рекурсивная функция)
-void printCodes(struct MinHeapNode* root, int arr[], int top) {
+void storeCodes(struct MinHeapNode* root, int arr[], int top, char* codes[]) {
     if (root->left) {
         arr[top] = 0;
-        printCodes(root->left, arr, top + 1);
+        storeCodes(root->left, arr, top + 1, codes);
     }
     if (root->right) {
         arr[top] = 1;
-        printCodes(root->right, arr, top + 1);
-    }
-    if (!(root->left) && !(root->right)) {
-        printf("%c: ", root->data);
-        for (int i = 0; i < top; ++i)
-            printf("%d", arr[i]);
-        printf("\n");
-    }
-}
-
-// Создание кодов Хаффмана и запись их в массив codes
-void storeCodes(struct MinHeapNode* root, int arr[], int top, char* codes[], int* freq) {
-    if (root->left) {
-        arr[top] = 0;
-        storeCodes(root->left, arr, top + 1, codes, freq);
-    }
-    if (root->right) {
-        arr[top] = 1;
-        storeCodes(root->right, arr, top + 1, codes, freq);
+        storeCodes(root->right, arr, top + 1, codes);
     }
     if (!(root->left) && !(root->right)) {
         codes[(unsigned char)root->data] = (char*)malloc(top + 1);
         for (int i = 0; i < top; ++i)
             codes[(unsigned char)root->data][i] = arr[i] + '0';
         codes[(unsigned char)root->data][top] = '\0';
-        freq[(unsigned char)root->data] = root->freq;
     }
 }
 
-// Сжатие файла
 void compressFile(const char* inputFileName, const char* archiveFileName) {
     FILE* inFile = fopen(inputFileName, "rb");
     if (!inFile) {
-        printf("Не удалось открыть файл для чтения\n");
+        perror("Cannot open file to read");
         return;
     }
 
-    int freq[256] = {0};
+    unsigned int freq[256] = {0};
     char ch;
-    while (fread(&ch, sizeof(char), 1, inFile))
+    while (fread(&ch, sizeof(char), 1, inFile)) {
         freq[(unsigned char)ch]++;
+    }
 
     fseek(inFile, 0, SEEK_SET);
 
-    char data[256];
-    int freqNonZero[256];
-    int size = 0;
-    for (int i = 0; i < 256; ++i) {
-        if (freq[i] > 0) {
-            data[size] = (char)i;
-            freqNonZero[size] = freq[i];
-            size++;
-        }
-    }
-
-    struct MinHeapNode* root = buildHuffmanTree(data, freqNonZero, size);
+    struct MinHeapNode* root = buildHuffmanTree(freq);
 
     char* codes[256] = {0};
     int arr[MAX_TREE_HT];
-    storeCodes(root, arr, 0, codes, freq);
+    storeCodes(root, arr, 0, codes);
 
     FILE* outFile = fopen(archiveFileName, "wb");
     if (!outFile) {
-        printf("Не удалось открыть файл для записи\n");
+        perror("Cannot open file to write");
+        fclose(inFile);
         return;
     }
 
-    fwrite(freq, sizeof(freq), 1, outFile);
+    if (fwrite(freq, sizeof(freq), 1, outFile) < 1) {
+        perror("Failed to write frequencies to file");
+        fclose(inFile);
+        fclose(outFile);
+        return;
+    }
 
-    unsigned char byte = 0;
+    unsigned char buffer = 0;
     int bitCount = 0;
     while (fread(&ch, sizeof(char), 1, inFile)) {
         char* code = codes[(unsigned char)ch];
         while (*code) {
-            byte = byte << 1 | (*code++ - '0');
+            buffer = (buffer << 1) | (*code++ - '0');
             bitCount++;
             if (bitCount == 8) {
-                fwrite(&byte, sizeof(byte), 1, outFile);
+                if (fwrite(&buffer, sizeof(char), 1, outFile) < 1) {
+                    perror("Failed to write data to file");
+                    fclose(inFile);
+                    fclose(outFile);
+                    return;
+                }
+                buffer = 0;
                 bitCount = 0;
             }
         }
     }
 
     if (bitCount > 0) {
-        byte <<= (8 - bitCount);
-        fwrite(&byte, sizeof(byte), 1, outFile);
+        buffer <<= (8 - bitCount);
+        if (fwrite(&buffer, sizeof(char), 1, outFile) < 1) {
+            perror("Failed to write the final bits to file");
+            fclose(inFile);
+            fclose(outFile);
+            return;
+        }
+        // Write the count of significant bits in the last byte
+        if (fwrite(&bitCount, sizeof(char), 1, outFile) < 1) {
+            perror("Failed to write the bit count to file");
+            fclose(inFile);
+            fclose(outFile);
+            return;
+        }
     }
 
     fclose(inFile);
@@ -216,49 +194,51 @@ void compressFile(const char* inputFileName, const char* archiveFileName) {
     }
 }
 
-// Распаковка файла
+
 void decompressFile(const char* archiveFileName, const char* outputFileName) {
     FILE* inFile = fopen(archiveFileName, "rb");
     if (!inFile) {
-        printf("Не удалось открыть файл для чтения\n");
+        perror("Cannot open file to read");
         return;
     }
 
     int freq[256] = {0};
-    fread(freq, sizeof(freq), 1, inFile);
-
-    char data[256];
-    int freqNonZero[256];
-    int size = 0;
-    for (int i = 0; i < 256; ++i) {
-        if (freq[i] > 0) {
-            data[size] = (char)i;
-            freqNonZero[size] = freq[i];
-            size++;
-        }
-    }
-
-    struct MinHeapNode* root = buildHuffmanTree(data, freqNonZero, size);
-
-    FILE* outFile = fopen(outputFileName, "wb");
-    if (!outFile) {
-        printf("Не удалось открыть файл для записи\n");
+    if (fread(freq, sizeof(freq), 1, inFile) < 1) {
+        perror("Failed to read frequencies from file");
+        fclose(inFile);
         return;
     }
 
-    struct MinHeapNode* current = root;
-    unsigned char byte;
-    while (fread(&byte, sizeof(byte), 1, inFile)) {
-        for (int i = 0; i < 8; ++i) {
-            if (byte & 0x80)
-                current = current->right;
-            else
-                current = current->left;
+    struct MinHeapNode* root = buildHuffmanTree(freq);
 
-            byte <<= 1;
+    FILE* outFile = fopen(outputFileName, "wb");
+    if (!outFile) {
+        perror("Cannot open file to write");
+        fclose(inFile);
+        return;
+    }
+
+    fseek(inFile, -1, SEEK_END); // Move to the last byte that contains the valid bits count
+    long dataSize = ftell(inFile) - sizeof(freq); // Calculate the data size
+
+    unsigned char lastByteInfo;
+    fread(&lastByteInfo, sizeof(char), 1, inFile); // Read the last byte to get the count of significant bits
+
+    fseek(inFile, sizeof(freq), SEEK_SET); // Move to the start of the encoded data
+
+    struct MinHeapNode* current = root;
+    int bit;
+    unsigned char byte;
+    long totalBits = (dataSize - 1) * 8 + lastByteInfo; // Adjusting the bits count
+
+    while (totalBits > 0 && fread(&byte, sizeof(byte), 1, inFile) == 1) {
+        for (int i = 0; i < 8; i++) {
+            if (--totalBits < 0) break; // Ensure we don't read past the valid bits
+            bit = (byte >> (7 - i)) & 1;
+            current = bit ? current->right : current->left;
 
             if (!current->left && !current->right) {
-                fwrite(&current->data, sizeof(current->data), 1, outFile);
+                fwrite(&current->data, sizeof(char), 1, outFile);
                 current = root;
             }
         }
@@ -268,10 +248,9 @@ void decompressFile(const char* archiveFileName, const char* outputFileName) {
     fclose(outFile);
 }
 
-// Главная функция
 int main(int argc, char* argv[]) {
     if (argc != 4) {
-        printf("Использование: %s <input_file> <-c|-x> <output_file>\n", argv[0]);
+        printf("Usage: %s <input_file> <-c|-x> <output_file>\n", argv[0]);
         return 1;
     }
 
@@ -280,7 +259,7 @@ int main(int argc, char* argv[]) {
     } else if (strcmp(argv[2], "-x") == 0) {
         decompressFile(argv[1], argv[3]);
     } else {
-        printf("Неизвестная команда: %s\n", argv[2]);
+        printf("Unknown command: %s\n", argv[2]);
         return 1;
     }
 
